7e511dcba7f0e007b0240c353a74d139061d19ea
p5eudoDjnn <alexander.havers@gmail.com>
2022-12-06 18:38:40 -0500

    added shopping cart with global state

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,22 +1,21 @@
-import React, { useEffect } from "react";
-import { useStoreContext } from "../../utils/GlobalState";
-import { useQuery } from "@apollo/client";
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { useStoreContext } from '../../utils/GlobalState';
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-} from "../../utils/actions";
-import { QUERY_CATEGORIES } from "../../utils/queries";
+} from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
+
   const { categories } = state;
+
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  // const categories = categoryData?.categories || [];
 
   useEffect(() => {
-    // if categoryData exsists or has changed from the response of useQuery, then run dispatch
     if (categoryData) {
-      // execute our dispatch function with out action object indicating the type of action and the data to set our state for categories to
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories,
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from "./reducers";
+import { useProductReducer } from './reducers'
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
@@ -7,11 +7,12 @@ const { Provider } = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
     products: [],
+    cart: [],
+    cartOpen: false,
     categories: [],
-    currentCategory: "",
+    currentCategory: '',
   });
-  // user this to confirm it works
-  console.log(state);
+
   return <Provider value={[state, dispatch]} {...props} />;
 };
 
